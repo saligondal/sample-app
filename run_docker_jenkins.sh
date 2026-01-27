f9dd1ca96e360b0f15a9313f4def3b800f2a0bcd
saligondal <sullaymanali.gondal@student.odisee.be>
2026-01-27 17:36:01 +0000

    sample app

--- run_docker_jenkins.sh
+++ run_docker_jenkins.sh
@@ -7,6 +7,8 @@ docker run -d \
 -p 8080:8080 \
 -p 50000:50000 \
 -v jenkins_home:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $(which docker):/usr/bin/docker \
 --name jenkins_server \
 jenkins/jenkins:lts
 
